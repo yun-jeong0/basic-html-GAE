b988c24fbf68ec3654b5588ca0a3c4e4b3b7d9c2
yun-jeong0 <kyun@sds.co.kr>
2024-11-26 17:28:09 +0900

    init

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,3 +1,20 @@
+// Add CSS styles dynamically
+const style = document.createElement('style');
+style.textContent = `
+.carousel {
+    width: 100%;
+    height: 200px;
+    overflow: hidden;
+}
+.carousel-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+`;
+document.head.append(style);
+
+// JavaScript for carousel functionality
 let currentIndex = 0;
 const images = document.querySelectorAll('.carousel-image');
 setInterval(() => {
